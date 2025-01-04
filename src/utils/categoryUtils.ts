cf74f8a99d1c05ddc6d63cc0954489207d1ba5d2
blova610 <blova610@gmail.com>
2025-01-04 17:26:45 +0700

    Delete src directory

--- src/utils/categoryUtils.ts
+++
@@ -1,7 +0,0 @@
-import { Game } from '../types/game';
-
-export const getUniqueCategories = (games: Game[]): string[] => {
-  const categories = games.map(game => game.category);
-  const uniqueCategories = Array.from(new Set(categories));
-  return uniqueCategories.sort();
-};
